5f7f22aa5e775279f2219055b6dc742a24c2483f
cramdesign <matt@cramdesign.com>
2015-03-29 14:56:38 -0400

    updates

---
+++ templates/performance-list.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	/* Template Name: Season Performances List */
+	
+	get_header();
+	
+	$layout = 'slats';
+	
+	include( locate_template( 'inc/performances.php' ) );
+		
+	get_footer(); 
+
+?>
+
+<!-- performance-list.php -->
