89535187aa395570b68cba8c40b0b92771a8136c
cramdesign <mcram@Cram-SSU-iMac.local>
2015-05-05 13:31:51 -0400

    small updates

--- inc/top.php
+++ inc/top.php
@@ -2,17 +2,12 @@
 <div class="row">
 
 	<nav>
-		<ul id="power-menu" class="menu">
-			<li><a href="http://shawnee.edu/">Shawnee.edu</a></li>
-			<li><a href="http://shawnee.edu/information/directory.aspx">Directory</a></li>
-			<li><a href="http://shawnee.edu/offices/information-technology-services/email-access.aspx">Webmail</a></li>
-			<li><a href="https://myssu.shawnee.edu/ics/">MySSU</a></li>
-			<li><a href="http://shawnee.edu/offices/">Offices</a></li>
-		</ul>
+		<a href="http://shawnee.edu/">Shawnee.edu</a>
+		<a href="http://shawnee.edu/information/directory.aspx">Directory</a>
+		<a href="http://shawnee.edu/offices/information-technology-services/email-access.aspx">Webmail</a>
+		<a href="https://myssu.shawnee.edu/">MySSU</a>
+		<a href="http://shawnee.edu/offices/">Offices</a>
 	</nav>
 	
-	<input type="checkbox" id="search-toggle" class="toggle"><label for="search-toggle" class="toggle"><span>Search</span></label>
-	<?php get_search_form(); ?>
-
 </div><!-- row -->
 </div><!-- top -->
