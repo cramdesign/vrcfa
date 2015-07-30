e0a480121e48e6530cca7ee7d19042b2cb05283b
cramdesign <mcram@Matts-iMac.local>
2015-07-30 08:18:23 -0400

    updates

--- functions/supersimple_shortcodes.php
+++ functions/supersimple_shortcodes.php
@@ -6,7 +6,7 @@
 
 	Description: WordPress shortcodes that are super simple
 
-	Version: 0.1.1
+	Version: 0.1.2
 
 */
 
@@ -18,14 +18,14 @@ function get_top_parent_page_id() {
     $ancestors = $post->ancestors;
  
     // Check if page is a child page (any level)
-    if ($ancestors) {
+    if ( $ancestors ) {
  
         //  Grab the ID of top-level page from the tree
-        return end($ancestors);
+        return end( $ancestors );
  
     } else {
  
-        // Page is the top level, so use  it's own id
+        // Page is the top level, so use it's own id
         return $post->ID;
  
     }
@@ -35,14 +35,13 @@ function get_top_parent_page_id() {
 
 
 // List current page and any child pages in a menu
-// [ss_list_pages]
 function supersimple_list_subpages() { 
 
 	global $post;
-	$parent_page = get_top_parent_page_id($post->ID);
 	
 	if ( is_page() ) :
 	
+		$parent_page = get_top_parent_page_id( $post->ID );
 		$childpages  = wp_list_pages( 'sort_column=menu_order&title_li=&include=' . $parent_page . '&echo=0' );
 		$childpages .= wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $parent_page . '&echo=0' );
 		
@@ -51,7 +50,7 @@ function supersimple_list_subpages() {
 	if ( $childpages ) return '<ul class="page_menu">' . $childpages . '</ul>';
 	
 }
-
+// [ss_list_pages]
 add_shortcode( 'ss_list_pages', 'supersimple_list_subpages' );
 
 
